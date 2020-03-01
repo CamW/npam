df9e98632390f0210fdc3e706347b09025be639e
Cameron Waldron <cameron.waldron@gmail.com>
2020-03-01 12:12:39 +0200

    Prevent error result for user delete.

--- test/setup_test_account.sh
+++ test/setup_test_account.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #WARNING! this script deletes a user named userxyz. Change the username here and in NpamTestsCommon.cs if you already have a user by that name on your system.
-sudo userdel userxyz
+sudo userdel userxyz || true
 sudo useradd -p $(perl -e 'print crypt($ARGV[0], "password")' "pwd123") userxyz
