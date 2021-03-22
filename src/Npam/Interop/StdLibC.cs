1bf99968535641125e96832f6ab0d487289fcc0d
Cameron Waldron <cwaldron@apple.com>
2021-03-22 15:44:00 +0200

    Remove library file extensions.

--- src/Npam/Interop/StdLibC.cs
+++ src/Npam/Interop/StdLibC.cs
@@ -5,13 +5,13 @@ namespace Npam.Interop
 {
     public class StdLibC {
         
-        [DllImport("libc.so.6")]
+        [DllImport("libc")]
         public static extern IntPtr getgrgid(int groupId);
 
-        [DllImport("libc.so.6")]
+        [DllImport("libc")]
         public static extern IntPtr getpwnam([MarshalAs(UnmanagedType.LPStr)] string user);
         
-        [DllImport("libc.so.6")]
+        [DllImport("libc")]
         public static extern int getgrouplist([MarshalAs(UnmanagedType.LPStr)] string user, int groupId, [Out] int[] groupIdArray, ref int numberOfGroups);
         
         public static AccountInfo GetPwNamAsAccountInfo(string user) {
